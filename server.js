e256e8c2bd5a0ce338b00766d2687905f68e626f
Sophia Chow <Sophia.t.chow@gmail.com>
2017-10-06 15:57:15 -0500

    created put route

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/', function(req,res){
   res.send('hello world');
 });
 
+//get all recipes
 app.get('/recipe', function(req,res){
   db.Recipe.find({}, function(err, data){
     if(err){
@@ -22,12 +23,57 @@ app.get('/recipe', function(req,res){
     }
   });
 });
-// or you can import route handlers from other files like this:
-// const testRoutes = require('./routes/test');
-// app.get('/test', testRoutes.getTestItems);
-// app.post('/test', testRoutes.createTestItem);
 
-// TODO: delete the above dummy routes and add your actual routes
+//post/create new recipe
+app.post('/recipe', function(req,res){
+  var newRecipe = new db.Recipe({
+    name: req.body.name,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    tags: req.body.tags
+  });
+  newRecipe.save(function(err,newRecipe){
+    if(err){
+      return console.log("create error: "+ err);
+    }
+    console.log("created ", newRecipe.name);
+    res.json(newRecipe);
+  });
+});
+
+//get one recipe by ID
+app.get('/recipe/:id', function(req, res){
+  var recipeId = req.params.id;
+  db.Recipe.findOne({_id: recipeId}, function(err, foundRecipe){
+    res.json(foundRecipe);
+  })
+});
+
+//update recipe
+app.put('/recipe/:id', function(req,res){
+  var recipeId = req.params.id;
+  var newRecipe = {
+    name: req.body.name,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    tags: req.body.tags
+  };
+  db.Recipe.findByIdAndUpdate(recipeId, newRecipe, {new: true}, function(err, updatedRecipe){
+    res.json(updatedRecipe);
+  });
+});
+
+//delete recipe
+app.delete('/recipe/:id', function(req,res){
+  var recipeId = req.params.idl
+  db.Recipe.findOneAndRemove({_id: recipeId}, function(err, removedRecipe){
+    res.json(removedRecipe)
+  })
+})
 
 // start app
 app.listen(port, function(err) {

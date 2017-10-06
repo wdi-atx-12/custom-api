118f89130b37b4b5f94f55e1990b04f491deecf4
Karla Moreno <moreno_karla@live.com>
2017-10-06 16:30:34 -0500

    delete unnecessary saved code

--- routes/routes.js
+++ routes/routes.js
@@ -10,18 +10,6 @@ function getUsers(req, res) {
     }
   });
 }
-// in server file/no route file (works)
-// get all users
-// app.get('/users', function(req, res) {
-//   db.User.find({}, function (err, data){
-//     if (err) {
-//       console.log("error retrieving users from db", err);
-//       res.status(500).send('Internal server error');
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// // });
 
 //(TODO: but i want all songs in a playlist)
 function getSongs(req, res) {
@@ -34,18 +22,6 @@ function getSongs(req, res) {
     }
   });
 }
-// in server file/no route file (works)
-// get all songs (TODO: but i want all songs in a playlist)
-// app.get('/songs', function(req, res) {
-//   db.Song.find({}, function (err, data){
-//     if (err) {
-//       console.log("error retrieving songs from db", err);
-//       res.status(500).send('Internal server error');
-//     } else {
-//       res.json(data);
-//     }
-//   });
-// });
 
 function getUserById(req, res){
   var userID = req.params.id;
@@ -58,20 +34,6 @@ function getUserById(req, res){
     }
   });
 }
-// in server file/no route file (works)
-// get user by ID
-// app.get('/users/:id', function(req, res) {
-//   var userID = req.params.id;
-//   db.User.findOne({_id: userID}, function (err, foundUser){
-//     if (err) {
-//       console.log("error retrieving users from db", err);
-//       res.status(500).send('Internal server error');
-//     } else {
-//       res.json(foundUser);
-//     }
-//   });
-// });
-
 
 // app.get('/users/:id', routeyRoutes.getUserById);
 
